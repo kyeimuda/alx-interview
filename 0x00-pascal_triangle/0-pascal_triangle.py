e99a14e972b0820fbb32cc172d93f7eecade758c
kyeimuda <kyeibernard01@gmail.com>
2022-06-20 01:51:48 +0000

    recode function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """returns a pascal triangle"""
 
@@ -8,10 +9,35 @@ def pascal_triangle(n):
 
     numberOfRows = n
     rowList = []
+    triangle = []
+    prevRow = []
 
+    
     for n in range(numberOfRows):
-        new = str(11 ** n)
+        if n == 0:
+            rowList = [1]
+            triangle.append(rowList)
+            
+        elif n == 1:
+            newRowColomn = rowList * (n+1)
+            prevRow = newRowColomn
+            triangle.append(newRowColomn)
+        elif n > 1:
+            newRowColomn = rowList * (n+1)
+            colomns = len(newRowColomn)
+            if newRowColomn[0] and newRowColomn[colomns - 1]:
+                newRowColomn[0] = 1
+                newRowColomn[colomns - 1] = 1
+            else:
+                for space in newRowColomn:
+                    start = 0
+                    if not (newRowColomn[0]) and not (newRowColomn[colomns - 1]):
+                        results = prevSpace[start] + prevSpace[start + 1]
+                        space = result
+                    start += 1
 
-        rowList.append(new)
-
-    return rowList
+                prevRow = newRowColomn
+                triangle.append(newRowColomn)
+                        
+            
+    return triangle
