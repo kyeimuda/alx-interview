4f108089a87661c45c56c2028c48017f4d592bb1
kyeimuda <kyeibernard01@gmail.com>
2022-06-20 06:01:28 +0000

    variable name corrections, comments, clean code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """returns a pascal triangle"""
 
+
 def pascal_triangle(n):
     """
     Returns a list of rows of the pascal triangke values
@@ -11,38 +12,37 @@ def pascal_triangle(n):
     triangle = []
     prevRow = []
 
-    
     for n in range(numberOfRows):
         if n == 0:
+            # runs for first row if triangle is one row
             rowList = [1]
             triangle.append(rowList)
-            
+
         elif n == 1:
-            newRowColomn = rowList * (n+1)
-            prevRow = newRowColomn
-            print(prevRow)
-            triangle.append(newRowColomn)
+            # runs for second row or if trinlangle is two rows
+            newRowColumn = rowList * (n+1)
+            prevRow = newRowColumn
+            triangle.append(newRowColumn)
 
         else:
-            print("===================")
-            newRowColomn = rowList * (n+1)
-            colomns = len(newRowColomn)
+            # runs for the rest of the rows or triangle is more than two rows
+            newRowColumn = rowList * (n+1)
+            columns = len(newRowColumn)
 
             i = 0
             start = 0
-            for space in newRowColomn:
-                
-                if (i == 0) or (i == colomns - 1):
-                    newRowColomn[0] = 1
-                    newRowColomn[colomns - 1] = 1
+            for space in newRowColumn:
+
+                if (i == 0) or (i == columns - 1):
+                    newRowColumn[0] = 1
+                    newRowColumn[columns - 1] = 1
                 else:
                     input = prevRow[start] + prevRow[start + 1]
-                    newRowColomn[i] = input
+                    newRowColumn[i] = input
                     start += 1
                 i += 1
 
-            prevRow = newRowColomn
-            triangle.append(newRowColomn)
-            
-                
+            prevRow = newRowColumn
+            triangle.append(newRowColumn)
+
     return triangle
