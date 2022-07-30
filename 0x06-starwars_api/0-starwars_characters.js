f73e8cc928674e32d44dacb7aa58fd205da7e343
kyeimuda <kyeibernard01@gmail.com>
2022-07-30 02:58:15 +0000

    star war api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 /**
  * Star wars api
- * Script that prints all characters of a Star Wars movie:
+ * Script that prints all characters of a Star Wars movie
  */
 const request = require('request');
 const filmId = process.argv[2];
