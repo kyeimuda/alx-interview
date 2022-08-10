29512b45b985ca8760447ac6349fc360002fac9b
kyeimuda <kyeibernard01@gmail.com>
2022-08-10 02:34:59 +0000

    no module import corrrection

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,24 +5,19 @@ Do not return anything. The matrix must be edited in-place.
 You can assume the matrix will have 2 dimensions and will not be empty.
 """
 
-import copy
-
 
 def rotate_2d_matrix(matrix):
     """
     2D matrix, rotate it 90 degrees clockwise function
     """
+    rows = len(matrix[0])
 
-    dupMatrix = copy.deepcopy(matrix)
-    lane = 0
+    dupmatrix = [[row[i] for row in matrix] for i in range(rows)]
 
-    for row in matrix:
-        index = (len(row) - 1)
-        indexTo = 0
+    matrix.clear()
 
-        for element in row:
-            change = dupMatrix[index][lane]
-            matrix[lane][indexTo] = change
-            index -= 1
-            indexTo += 1
-        lane += 1
+    for row in dupmatrix:
+        row.reverse()
+
+    for row in dupmatrix:
+        matrix.append(row)
