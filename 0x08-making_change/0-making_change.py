f708d733e7edd80680dd1d5b2d4a2ded37303df4
kyeimuda <kyeibernard01@gmail.com>
2022-08-12 04:08:37 +0000

    making change recode

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 """
-Given a pile of coins of different values, determine the
-fewest number of coins needed to meet a given amount total.
+Given a pile of coins of different values, determine the fewest
+ number of coins needed to meet a given amount total.
 """
 
 
 def makeChange(coins, total):
     """
-    Determines the fewest coins to meet total
-    Args:
-        coins (list): list of coins
-        total (int): value to meet as total
-    Returns:
-        coins (int): fewest number of coins needed to meet total
-        (0) if total is 0 or less
-        (-1) if no number of the coins can meet the total
+    This function takes a pile of coins of different values and determine
+     the fewest  number of coins needed to meet a given amount total.
     """
+
+    coins.sort()
+
+    changeAddUp = total
+    coinCount = 0
+    numOfCoins = len(coins)
+
     if total <= 0:
         return 0
-    min_coins = [0] + [float('inf')] * total
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                num_coins = min_coins[i - coin] + 1
-                if num_coins < min_coins[i]:
-                    min_coins[i] = num_coins
-    if min_coins[-1] == float("inf"):
+
+    while numOfCoins > 0:
+        if (changeAddUp - coins[numOfCoins - 1]) >= 0:
+            changeAddUp = changeAddUp - coins[numOfCoins - 1]
+            coinCount = coinCount + 1
+        else:
+            numOfCoins = numOfCoins - 1
+
+    if changeAddUp == 0:
+        return coinCount
+    else:
         return -1
-    return min_coins[total]
